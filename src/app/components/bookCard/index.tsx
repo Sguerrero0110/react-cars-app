394eec6ca26c5bf9f69569fc0b03ed426c0c6309
Steven <sguerrero0110@yahoo.com>
2022-07-17 14:04:02 -0500

    calender added

--- src/app/components/bookCard/index.tsx
+++ src/app/components/bookCard/index.tsx
@@ -1,10 +1,17 @@
-import { faCalendarAlt } from '@fortawesome/free-solid-svg-icons';
+import {
+  faCalendarAlt,
+  faCaretDown,
+  faCaretUp,
+} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react';
-import styled from 'styled-components';
+import React, { useState } from 'react';
+import styled, { css } from 'styled-components';
 import tw from 'twin.macro';
 import { Button } from '../button';
 import { Marginer } from '../marginer';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+import { SCREENS } from '../responsive';
 
 const CardContainer = styled.div`
   min-height: 4.3em;
@@ -28,7 +35,7 @@ const CardContainer = styled.div`
 
 const ItemContainer = styled.div`
   ${tw`
-    flex
+    flex relative
   `};
 `;
 
@@ -43,11 +50,23 @@ const Icon = styled.span`
   `};
 `;
 
+const SmallIcon = styled.span`
+  ${tw`
+    text-gray-500
+    fill-current
+    text-xs
+    md:text-base
+    ml-1
+  `};
+`;
+
 const Name = styled.span`
   ${tw`
     text-gray-600
     text-xs
     md:text-sm
+    cursor-pointer
+    select-none
   `};
 `;
 
@@ -63,21 +82,77 @@ const LineSeperator = styled.span`
   `};
 `;
 
+const DateCalendar = styled(Calendar)`
+  position: obsolute;
+  max-width: none;
+  user-select: none;
+  top: -2em;
+  left: 0;
+
+  ${({ offset }: any) =>
+    offset &&
+    css`
+      left: -6em;
+    `};
+
+  @media (min-width: ${SCREENS.md}) {
+    top: 3.5em;
+    left: -2em;
+  }
+` as any;
+
 export function BookCard() {
+  const [startDate, setStartDate] = useState<Date>(new Date());
+  const [isStartCalendarOpen, setStartCalendarOpen] = useState(false);
+  const [returnDate, setReturnDate] = useState<Date>(new Date());
+  const [isReturnCalendarOpen, setReturnCalendarOpen] = useState(false);
+
+  const toggleStartDateCalendar = () => {
+    setStartCalendarOpen(!isStartCalendarOpen);
+    if (isReturnCalendarOpen) setReturnCalendarOpen(false);
+  };
+
+  console.log('value', startDate);
+
+  const toggleReturnDateCalendar = () => {
+    setReturnCalendarOpen(!isReturnCalendarOpen);
+    if (isStartCalendarOpen) setStartCalendarOpen(false);
+  };
+
   return (
     <CardContainer>
       <ItemContainer>
         <Icon>
           <FontAwesomeIcon icon={faCalendarAlt} />
         </Icon>
-        <Name>Pick Up Date</Name>
+        <Name onClick={toggleStartDateCalendar}>Pick Up Date</Name>
+        <SmallIcon>
+          <FontAwesomeIcon
+            icon={isStartCalendarOpen ? faCaretUp : faCaretDown}
+          />
+        </SmallIcon>
+        {isStartCalendarOpen && (
+          <DateCalendar value={startDate} onChange={setStartDate as any} />
+        )}
       </ItemContainer>
       <LineSeperator />
       <ItemContainer>
         <Icon>
           <FontAwesomeIcon icon={faCalendarAlt} />
         </Icon>
-        <Name>Return Date</Name>
+        <Name onClick={toggleReturnDateCalendar}>Return Date</Name>
+        <SmallIcon>
+          <FontAwesomeIcon
+            icon={isReturnCalendarOpen ? faCaretUp : faCaretDown}
+          />
+        </SmallIcon>
+        {isReturnCalendarOpen && (
+          <DateCalendar
+            offset
+            value={returnDate}
+            onChange={setReturnDate as any}
+          />
+        )}
       </ItemContainer>
       <Marginer direction='horizontal' margin='2em' />
       <Button text='Book Your ride' theme={'filled'} />
